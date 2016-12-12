60c04e44f797ce4b2fd1db6009f2b947a44f6b15
sseaver <sseaver321@gmail.com>
2016-12-12 14:19:54 -0500

    initial commit

---
+++ .direnv/python-3.5.2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sseaver/tiy-projects/sseaver.us/.direnv/python-3.5.2/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
