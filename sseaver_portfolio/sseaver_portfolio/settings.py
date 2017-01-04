28bc1055e820058dd7dfb47d2b34f9f252793d18
sseaver <sseaver321@gmail.com>
2017-01-04 14:32:00 -0500

    added html and css files

--- sseaver_portfolio/sseaver_portfolio/settings.py
+++ sseaver_portfolio/sseaver_portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'sseaver_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'app/static/'
+MEDIA_URL = 'app/static/img/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
